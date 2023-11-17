9ef1e3034ac92c870924592111efde4e900e861a
kimganghyeon <kimganghyeon6080@naver.com>
2023-11-17 21:36:53 +0900

    Delete week10(11-16) directory

--- week10(11-16)/ajax-html.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {  
-	$('#btnLoad3').click( function() {
-		$('#listArea').empty();					
-		$('#listArea').load('ajax-listinfo.html li', function(htmlData){		
-			$('#listArea').listview('refresh');	
-		});			
-	});
-});
